dccdabe045c86d0521adb770ccf581bec2f3f02f
Ashok it <vishalmorey1998@gmail.com>
2025-04-11 17:04:33 +0530

    someChanges

--- src/main/java/com/dena/demo/controller/DenaBankController.java
+++ src/main/java/com/dena/demo/controller/DenaBankController.java
@@ -6,12 +6,20 @@ public ResponseEntity<Dena> getAllData()
 {
 return new ResponseEntity<Dena>(http.Status.Ok);
 }
+@PostMapping("/denaUserInfo")
+public ResponseEntity<Dena> saveInfoData()
+{
+return new ResponseEntity<>
+}
 
 @PutMapping("/update/{udi}")
 public ResponseEntity<Dena> updateByUserId()
-
 {
 return new ResponseEntity<Dena>(http.Status.Ok)
 }
+
+
+
+
 
 }
